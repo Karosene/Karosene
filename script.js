801914ba262db47cb724888845192bf48a866fff
Karosene <146135871+Karosene@users.noreply.github.com>
2023-12-18 07:21:02 +0100

    Update script.js
    to rock paper scissors

--- script.js
+++ script.js
@@ -12,7 +12,59 @@ function appendCharacter(char) {
 }
 
 function deleteChar() {
-    if (display.value == 'Error') {
+    if (display.value == 'Error') {let countDraw = 0;
+let countLose = 0;
+let countWin = 0;
+let gameCount = 0;
+
+function startGame() {
+    let choice = prompt("Game begins. Write ROCK, PAPER, or SCISSORS.").toLowerCase();
+    if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+        var pcChoice = pcInput();
+        alert("Your choice: " + choice)
+        alert("Randomly generated prompt: " + pcChoice)
+
+        var finalAns = answer(choice, pcChoice);
+        alert(finalAns);
+        
+        gameCount++;
+
+        updateScores();
+
+        console.log(choice, pcChoice, finalAns)
+    } else {
+        alert("Invalid choice");
+    }
+}
+
+function pcInput() {
+    var options = ['rock', 'paper', 'scissors'];
+    var index = Math.floor(Math.random() * 3);
+    return options[index];
+}
+
+function answer(choice, pcChoice) {
+    if (choice === pcChoice) {
+        countDraw++;
+        return 'Draw';
+    } else if (
+        (choice === 'rock' && pcChoice === 'scissors') ||
+        (choice === 'paper' && pcChoice === 'rock') ||
+        (choice === 'scissors' && pcChoice === 'paper')
+    ) {
+        countWin++;
+        return 'You Won!';
+    } else {
+        countLose++;
+        return 'You Lose.';
+    }
+}
+
+function updateScores() {
+    document.getElementById("scoreUser").innerText = "User: " + countWin;
+    document.getElementById("scoreComputer").innerText = "Computer: " + countLose;
+    document.getElementById("scoreDraw").innerText = "Draw: " + countDraw;
+}
         display.value = '0';
     }
     else if (display.value.length === 1) {
